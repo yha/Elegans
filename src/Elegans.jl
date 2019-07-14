9b88ce1c8bef965c5c498d122eab01d0dba673db
yha <yharel@campus.technion.ac.il>
2019-07-15 01:00:48 +0300

    new exports

--- src/Elegans.jl
+++ src/Elegans.jl
@@ -1,6 +1,7 @@
 module Elegans
 
-export mark_stages_gui, mark_stages_window
+export import_coords, import_and_calc, loadstages, stage_names,
+       mark_stages_gui, mark_stages_window, frames_per_s
 
 include("mark_stages.jl")
 
