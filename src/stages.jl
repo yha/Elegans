09ced0f8cb1954253ef2f89540f41ef77be102fe
yha <yharel@campus.technion.ac.il>
2019-08-18 23:48:55 +0300

    Remove spans function which accidentally relied on non-avalable globals

--- src/stages.jl
+++ src/stages.jl
@@ -6,11 +6,6 @@ loadstages() = TOML.parsefile(stages_filepath)
 savestages(stages) = open( io->TOML.print(io,stages), stages_filepath, "w" )
 #appendstages(stages...) = open( io->TOML.print(io,Dict(stages)), stages_filepath, "a" )
 
-function spans( stages, ex, cam )
-    frames_per_hr = frames_per_s * 3600
-    n = nrow(worms[ex][cam])
-    Iterators.partition(round.(Int, [stages[ex][cam] .* frames_per_hr; n]), 2)
-end
 
 ##
 
