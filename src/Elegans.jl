0e27071c4dc1533393a6eebf22ac8d0662c676a3
yha <yharel@campus.technion.ac.il>
2019-12-18 01:27:27 +0200

    exports

--- src/Elegans.jl
+++ src/Elegans.jl
@@ -1,8 +1,20 @@
 module Elegans
 
 export import_coords, import_and_calc, loadstages, stage_names,
-       mark_stages_gui, mark_stages_window, frames_per_s
+       VideoCache, get_frame, nframes,
+       mark_stages_gui, mark_stages_window, frames_per_s,
+       raw_worm_counts, worm_contour, curvature, #circfilter, circfilter!,
+       incurve, cutline, joincurves,
+       Closed2DCurve, n_highest_peaks_circular,
+       midline_by_thinning, line2spline, spline_anglevec,
+       anglevec_by_thinning, midline_cache, spline_cache,
+       ends_alignment_mask, align_ends!
 
 include("mark_stages.jl")
+include("shape.jl")
+include("contour.jl")
+include("midline.jl")
+include("peaks.jl")
+include("videocache.jl")
 
 end # module
