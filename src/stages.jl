7b5eced7c5d576ab571f5106ef78aa520554e947
yha <yharel@campus.technion.ac.il>
2019-12-17 17:43:44 +0200

    Custom stages file

--- src/stages.jl
+++ src/stages.jl
@@ -2,8 +2,8 @@ using TOML
 
 const stage_names = ["L1", "L2", "L3", "L4", "A"]
 const stages_filepath = normpath("$(@__DIR__)/../stages.toml")
-loadstages() = TOML.parsefile(stages_filepath)
-savestages(stages) = open( io->TOML.print(io,stages), stages_filepath, "w" )
+loadstages(stagefile=stages_filepath) = TOML.parsefile(stagefile)
+savestages(stages, stagefile=stages_filepath) = open( io->TOML.print(io,stages), stagefile, "w" )
 #appendstages(stages...) = open( io->TOML.print(io,Dict(stages)), stages_filepath, "a" )
 
 
