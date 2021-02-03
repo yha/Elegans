ecbeed1a7053c1cd8c73f7a2c3d666c7ac470d19
yha <yharel@campus.technion.ac.il>
2021-02-03 20:56:03 +0200

    video prefetch. Faster VideoCache construction with traj parameter.

--- src/videocache.jl
+++ src/videocache.jl
@@ -5,28 +5,53 @@ struct VideoCache{F}
     boundaries::Vector{Int}
     cache::LRU{Int,Any}
 
-    function VideoCache( cam, datadir = Elegans.datadir, maxsize = 20 )
+    # TODO change signature to (ex, cam, datadir) to allow using `load_coords_and_size`
+    function VideoCache( cam, datadir = Elegans.datadir; traj = nothing, maxsize = 20 )
         path_f = Elegans.videopath_f(cam, datadir)
-        _, boundaries = import_coords(cam, datadir)
+        if traj === nothing
+            _, boundaries = import_coords(cam, datadir)
+        else
+            boundaries = boundaries_from_traj(traj)
+        end
         cache = LRU{Int,Any}( maxsize = maxsize )
         new{typeof(path_f)}( path_f, boundaries, cache )
     end
 end
 
-function get_frame(cache::VideoCache, i)
+function globalframe_to_fileframe(cache::VideoCache, i)
     fileno = searchsortedfirst(cache.boundaries, i) - 1
     file_offset = fileno == 0 ? 0 : cache.boundaries[fileno]
-    # @show (fileno,i-file_offset)
+    fileno, i - file_offset
+end
+
+function _fetch_video(cache::VideoCache, fileno)
     frames = get!(cache.cache, fileno) do
-        @info "Reading file $fileno" _group=:videoread
+        @info "Reading video $fileno ($(cache.path_f(fileno)))" _group=:videoread
         Elegans.read_video( cache.path_f, fileno )
     end
-    frame = frames[i-file_offset]
+end
+
+function get_frame(cache::VideoCache, i)
+    fileno, frameno = globalframe_to_fileframe(cache, i)
+    frames = _fetch_video(cache, fileno)
+    frame = frames[frameno]
     # Frame should have odd size, but may be padded to even size
     # due to encoding limitations. Undo padding.
     frame = frame[1:end-mod((size(frame,1)+1),2),
                   1:end-mod((size(frame,2)+1),2)]
 end
+
+function prefetch_framerange(cache::VideoCache, from, to)
+    from_file, _ = globalframe_to_fileframe(cache, from)
+    to_file, _   = globalframe_to_fileframe(cache, to)
+    @info "Prefetching video files $(from_file)—$to_file"
+    for k in from_file:to_file
+        _fetch_video(cache, k)
+        yield()
+        #sleep(0.05)
+    end
+end 
+prefetch_framerange(cache::VideoCache, r::AbstractUnitRange) = prefetch_framerange(cache, first(r), last(r))
 
 nframes(cache::VideoCache) = cache.boundaries[end]
 
