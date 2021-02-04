c5e9b1713e4178822a41e26921a8969c0565f652
yha <yharel@campus.technion.ac.il>
2021-02-04 16:38:28 +0200

    roaming fraction in centered windows

--- src/roam_fraction.jl
+++ src/roam_fraction.jl
@@ -1,21 +1,21 @@
-using RollingFunctions
+using ImageFiltering
 
 const binsize_speed = 6
 const binsize_da = deg2rad(3.6)
 const roaming_slope_per_stage = [5, 2.5, 2.3, 2, 1.5]
+const runmean_kernel = (len = 10*frames_per_s + 1; centered(fill(1/len,len)))
 
-# TODO fix to smooth in centered windows (no lag), matching MATLAB
+m2n(v) = replace(v, missing=>NaN)
 _isroaming(speed,dangle,slope) = slope*(speed/binsize_speed + 1) > dangle/binsize_da + 1
 function isroaming(traj, rows, slope)
-    smooth_window = 10*frames_per_s + 1
-    smspeed = runmean(traj.speed[rows], smooth_window)
-    smdangle = runmean(abs.(traj.dangle[rows]), smooth_window)
+    smspeed = imfilter(m2n(traj.speed[rows]), runmean_kernel)
+    smdangle = imfilter(m2n(abs.(traj.dangle[rows])), runmean_kernel)
     _isroaming.(smspeed, smdangle, slope)
 end
 
 function roam_for_stage(ex, cam, traj, stage; stagedict=loadstages())
     slope = roaming_slope_per_stage[stage]
     idxs = stagedict[ex][cam]
-    rows = idxs[stage]:idxs[stage+1]
+    rows = idxs[stage]+1:idxs[stage+1]
     isroaming(traj, rows, slope)
 end
