7778f2357d1f6685c234b2d0390980637efe253c
yha <yharel@campus.technion.ac.il>
2021-02-16 23:21:29 +0200

    fix double definition of `m2n`

--- src/roam_fraction.jl
+++ src/roam_fraction.jl
@@ -5,7 +5,6 @@ const binsize_da = deg2rad(3.6)
 const roaming_slope_per_stage = [5, 2.5, 2.3, 2, 1.5]
 const runmean_kernel = (len = 10*frames_per_s + 1; centered(fill(1/len,len)))
 
-m2n(v) = replace(v, missing=>NaN)
 _isroaming(speed,dangle,slope) = slope*(speed/binsize_speed + 1) > dangle/binsize_da + 1
 function isroaming(traj, rows, slope)
     smspeed = imfilter(m2n(traj.speed[rows]), runmean_kernel)
