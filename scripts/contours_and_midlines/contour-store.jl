d89d3c419a52c55fe487eda86c1789b3344aa69b
yha <yharel@campus.technion.ac.il>
2024-02-26 19:30:07 +0200

    contour-store.jl env and script

---
+++ scripts/contours_and_midlines/contour-store.jl
@@ -0,0 +1,251 @@
+using Pkg
+
+local_project_root = @__DIR__
+
+Pkg.activate(local_project_root)
+@info "Installing packages..."
+Pkg.instantiate()
+
+##
+using TOML
+
+args = TOML.parsefile("$(@__DIR__)/args.toml")
+paths, dist = args["paths"], args["dist"]
+
+stage_path   = normpath(joinpath(local_project_root, paths["stage_path"]))
+
+local_root = paths["local_root"]
+logdir = paths["logdir"]
+
+remote_root = dist["remote_root"]
+remote_project_root = "$remote_root/$(dist["remote_project_root"])"
+
+ex_dir        = paths["ex_dir"]
+contours_dir  = paths["contours_dir"]
+
+local_ex_path       = normpath(joinpath(local_root, ex_dir))
+local_contours_path = normpath(joinpath(local_root, contours_dir))
+
+assert_isdir(path)  = isdir(path)  || error("Not found or not a directory: $path")
+assert_isfile(path) = isfile(path) || error("Not found or not a regular file: $path")
+
+assert_isdir(local_root)
+assert_isdir(local_project_root)
+assert_isdir(local_ex_path)
+assert_isdir(local_contours_path)
+assert_isfile(stage_path)
+
+skip_existing = args["params"]["skip_existing"]
+
+##
+
+using Elegans
+using Distributed
+
+
+contour_methods = Dict( 1 => Thresholding(1.0,0.34), 
+    (i => Thresholding(1.0,0.35) for i in 2:5)...
+)
+
+contour_method2stages = Dict(
+    (method => sort([stage for (stage,m) in contour_methods if m == method])) for method in unique(values(contour_methods))
+)
+
+# contour_method = Thresholding(1.0,0.34)
+# stages = nothing
+# #stages = 1:5
+# # contour_method = Thresholding(1.0,0.35)
+# # stages = 1:1
+
+allstages = loadstages(stage_path)
+
+##
+
+exs = readdir(local_ex_path)
+
+filter!(in(keys(allstages)), exs)
+
+@info "Found $(length(exs)) experiments (directories in `$local_ex_path` with entry at `$stage_path`)"
+
+exwells = [(ex,well) for ex in exs for well in sort!(collect(keys(allstages[ex])))]
+
+@info "...$(length(exwells)) wells"
+
+##
+
+#using DataStructures
+
+
+
+##
+
+using Distributed
+
+remote = dist["remote"]
+n_local, n_remote = dist["n_local"], dist["n_remote"]
+
+@info "Starting workers..." remote_root remote_project_root local_root local_project_root n_local n_remote
+
+rmprocs(workers()...)
+local_procs = cd(local_project_root) do
+    addprocs(n_local; exeflags = "--project")
+end
+remote_procs = n_remote == 0 ? Int[] :
+                addprocs([(remote, n_remote)];
+                      dir = remote_project_root, 
+                      exename = "$remote_root/julia-$VERSION/bin/julia",
+                      exeflags = "--project"
+                      )
+
+# ##
+# @everywhere using Pkg
+
+# println(join(
+#     map(procs()) do i
+#         s = remotecall_fetch(()->sprint(io->Pkg.status(;io)), i)
+#         "Worker $i:\n$s"
+#     end, 
+#     "\n"))
+
+
+##
+
+@everywhere function contour_counts(well_data, nbins)
+    n = length(well_data.traj.x)
+    bin_edges = round.(Int, range(0, n; length=nbins+1))
+    completed = sort!(collect(keys(well_data.contours.cache)))
+    cumcounts = [searchsortedfirst( completed, edge ) for edge in bin_edges]
+    counts = diff(cumcounts)
+    counts, bin_edges
+end
+
+
+##
+
+@everywhere using ObservablePmap
+
+##
+
+@everywhere using IterTools
+@everywhere begin
+    block_repr(p, empty="\u2591") = (r=round(Int,8p)) >= 8 ? "█" : r <= 0 ? empty : '\u2580' + r
+    _stage_str(len, name) = let s = max(len-1-length(name),0)
+        #"\\" * "⋅"^(s÷2) * name * "⋅"^((s+1)÷2) * "/"
+        " "^(s÷2) * name * " "^((s+1)÷2)
+    end
+    function cam_progress_str( well_data, nbins, stage_ends, frames )
+        counts, bin_edges = contour_counts(well_data, nbins)
+        binlens = diff(bin_edges)
+        #rep(p) = (r=round(Int,10p)) >= 10 ? "#" : string(r)
+        bin_required = [!isempty(intersect(frames,bin)) for bin in IterTools.partition(bin_edges,2,1)]
+        progline = join((block_repr( c/l, r ? " " : "\u2591" )
+                        for (c,l,r) in zip(counts,binlens,bin_required)))
+
+        n = length(well_data.traj.x)
+        stage_edge_bins = round.(Int, stage_ends .* (nbins/n))
+        #@show stage_ends stage_edge_bins
+        stageline = join((_stage_str(d,n) for (d,n) in zip(
+                        diff([0;stage_edge_bins]), [" "; Elegans.stage_names])), "|") * "|"
+        progline * "\n" * stageline * "\n"
+    end
+end
+
+##
+
+
+using Elegans
+@everywhere using Elegans
+#@everywhere include(joinpath(pwd(),"scripts/midline/midline-stats-funcs.jl"))
+
+##
+
+@everywhere using DataStructures
+@everywhere using ProgressLogging
+@everywhere using TerminalLoggers
+
+@everywhere function store_contours_remote(well_data, stage_ends, frames; chunklen = 1000)
+    set_status(str) = @info str
+
+    computed = collect(keys(well_data.contours.cache))
+    todo = setdiff!(OrderedSet(frames), computed)
+
+    set_status("Computing $(length(todo)) out of $(length(frames)) contours.")
+
+    for (i,c) in enumerate(todo)
+        well_data.contours(c)
+        if mod(i,chunklen) == 0
+            str = cam_progress_str(well_data, 100, stage_ends, frames)
+            set_status("Contours progress: \n" * str)
+        end
+    end
+    if isempty(todo)
+        set_status("No new contours computed.")
+    else
+        set_status("Saving contours to $(well_data.contours_file)...")
+        save_contours(well_data.contours, well_data.contours_file)
+        set_status("Done.")
+    end
+end
+
+
+##
+
+well_method_combinations = Iterators.product(exwells, keys(contour_method2stages))
+
+using WebIO, CSSUtil, Mux
+@everywhere using Logging: current_logger, with_logger
+@everywhere using LoggingExtras: EarlyFilteredLogger
+
+#summ, task = ologpmap(exwells; on_error=identity) do (ex, wellname)
+summ, task = ologpmap(well_method_combinations; on_error=identity) do ((ex, wellname), contour_method)
+    with_logger(EarlyFilteredLogger( log -> log.group != :videoread, current_logger() )) do
+
+        isremote = myid() in remote_procs
+        root = isremote ? remote_root : local_root
+
+        exps_path = joinpath( root, ex_dir )
+        contours_path = joinpath( root, contours_dir )
+        #@show exps_path contours_path
+        @assert isdir(joinpath(exps_path,ex))
+        well = Well(exps_path, ex, wellname)
+        well_str = "$(well.experiment): $(well.well)"
+    
+        @info "Loading $well_str..."
+        if skip_existing
+            contours_file = Elegans.contours_filename( well, contour_method, contours_path )
+            if isfile(contours_file)
+                @info "Skipping existing contour file: $contours_file"
+                return
+            end
+        end
+
+        traj = load_coords_and_size(well)
+        contours, contours_file, vcache = init_contours( well, contour_method, contours_path; traj )
+        well_data = (; contours, contours_file, traj)
+
+        @info "... loaded $well_str."
+        stage_ends = allstages[well.experiment][well.well]
+        stage_ranges = [x+1:y for (x,y) in IterTools.partition(stage_ends,2,1)]
+        stages = contour_method2stages[contour_method]
+        stage_i = something(stages, 1:length(stage_ends)-1)
+        frames = union(stage_ranges[stage_i]...)
+        store_contours_remote( well_data, stage_ends, frames )
+    end
+end
+
+h = map(x -> style(HTML("<pre>$x</pre>")), summ)
+
+port = rand(8100:8200)
+WebIO.webio_serve(page("/", vbox(h)), port)
+address = "localhost:$port"
+println()
+println("Progress available at $address (copied to clipboard)")
+clipboard(address)
+
+Threads.@spawn begin
+    wait(task)
+    @info "Done"
+end
+
+nothing
+
